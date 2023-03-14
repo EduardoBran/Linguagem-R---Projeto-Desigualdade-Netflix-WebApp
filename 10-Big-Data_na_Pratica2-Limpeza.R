f0310a8979ea4c22a47d137828acf96b4564d0a8
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-03-14 17:59:09 -0300

    limpeza e organizacao de diferentes fontes de dados - parte 2

--- 10-Big-Data_na_Pratica2-Limpeza.R
+++ 10-Big-Data_na_Pratica2-Limpeza.R
@@ -54,44 +54,55 @@ View(countrycode)
 
 
 
+# *** Análise exploratória (não foi feita para esse exemplo) ***
+
+
+
 
 # *** Limpeza e Preparação do Primeiro Dataset Combinado ***
 
-# Cria uma coluna com a diferença de dados para o gráfico de barras (plano standard - plano basico)
+
+# Cria uma coluna com a diferença de dados do para o gráfico de barras (plano standard - plano basico)
 dados_netflix$basic_standard_diff = (dados_netflix$Cost.Per.Month...Standard.... - dados_netflix$Cost.Per.Month...Basic....)
 
 # Cria uma coluna com a diferença de dados para o gráfico de barras (plano premium - plano standard)
 dados_netflix$standard_premium_diff = (dados_netflix$Cost.Per.Month...Premium.... - dados_netflix$Cost.Per.Month...Standard....)
 
+View(dados_netflix)
 
 # Combina os dados anteriores com dados do PIB
 
-# renomeando para 'Country' a primeira coluna
+# renomeando para 'Country' a primeira coluna de 'dados_pib'
 names(dados_pib)[names(dados_pib) == 'V1'] <- 'Country' 
 
-# a função merge() é utilizada para combinar os conjuntos de dados dados_netflix e dados_pib com base na coluna Country. Isso
-# cria um novo conjunto de dados chamado dados_netflix_pib, que contém todas as informações dos dois conjuntos de dados
+# A função merge() é utilizada para combinar os conjuntos de dados 'dados_netflix' e 'dados_pib' com base na coluna Country.
+# Isso cria um novo conjunto de dados chamado dados_netflix_pib, que contém todas as informações dos dois conjuntos de dados
 # originais, combinadas em uma única tabela. A combinação é feita de tal forma que apenas as linhas que possuem o mesmo valor
 # na coluna Country em ambos os conjuntos de dados são mantidas.
 dados_netflix_pib <- merge(dados_netflix, dados_pib, by = 'Country')
+
 View(dados_netflix_pib)
 
-View(dados_netflix)
 
 # Extrai o PIB de 2020
 
-# exclui todas as colunas de 11 a 72 e 74 e 75 e cria um novo df. Essas colunas correspondem a dados dos anos anteriores a 2020
+# exclui todas as colunas de 11 a 72 e 74 e 75 de 'dados_netflix_pib' e cria um novo df. Essas colunas
+# correspondiam aos dados dos anos anteriores a 2020
 dados_netflix_pib2020 <- dados_netflix_pib[-c(11:72, 74, 75)]
+View(dados_netflix_pib2020)
 
 # renomeando o nome da coluna
-names(dados_netflix_pib2020)[names(dados_netflix_pib2020) == 'V65'] <- '2020 GDP (World Bank)'
+names(dados_netflix_pib2020)[names(dados_netflix_pib2020) == 'V64'] <- '2020 GDP (World Bank)'
 View(dados_netflix_pib2020)
 
 
-# Limpeza do dataframe de desigualdade salarial ("%>%" simbolo para concatenar comandos)
+
+# Limpeza do dataframe de desigualdade salarial ( "%>%" simbolo para concatenar comandos )
 
 # remove todas as colunas a partir da quarta coluna em diante e mantém apenas as três primeiras colunas do conjunto de dados.
 dados_salario <- dados_salario[, c(1:3)]
+
+View(dados_salario)
 
 # Cria um novo df onde ele utiliza o operador %>% (pipe) para direcionar a saída do objeto dados_salario para a próxima função.
 # Em seguida, o group_by(country) agrupa os dados de salário pelo nome do país.
@@ -101,4 +112,26 @@ dados_salario <- dados_salario[, c(1:3)]
 dados_salario_ano <- dados_salario %>% group_by(country) %>% summarise(max = max(year, na.rm = TRUE))
 
 View(dados_salario_ano)
-View(dados_salario)
+
+
+# Combina os dataframes
+
+# - Combinando os dois df dados_salario e dados_salario_ano usando o país usando a funcao merge().
+# - O argumento by.x especifica quais colunas da tabela dados_salario devem ser usadas como chave de junção. Nesse caso, as
+#   colunas são country e year. O argumento by.y especifica quais colunas da tabela dados_salario_ano devem ser usadas como
+#   chave de junção. Nesse caso, as colunas são country e max.
+# - Assim, a tabela resultante dados_salario2 contém informações sobre o índice de desigualdade de renda (gini_disp) para cada
+#   país e ano presente na tabela dados_salario, juntamente com o ano máximo presente na tabela dados_salario_ano.
+# - Caso haja países com anos diferentes entre as tabelas, os registros desses países serão descartados na junção. Já se
+#   houver anos diferentes para um mesmo país nas tabelas, os registros serão mantidos e apresentados na tabela final.
+
+dados_salario2 <- merge(dados_salario, dados_salario_ano, by.x = c('country', 'year'), by.y = c('country', 'max'))
+View(dados_salario2)
+
+# - Realiza um merge() entre dois data frames: dados_netflix_pib2020 e dados_salario2, baseado na coluna Country 
+#   em dados_netflix_pib2020 e na coluna country em dados_salario2.
+
+dados_netflix_pib_salario2020 <- merge(dados_netflix_pib2020, dados_salario2, by.x = c('Country'), by.y = c('country'))
+View(dados_netflix_pib_salario2020)
+
+#
